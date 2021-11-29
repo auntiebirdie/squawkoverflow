222d69a9279f80f5c0c601b29060ebe257748bf3
seppukawaii <seppukawaii@gmail.com>
2021-11-29 07:51:29 +0000

    Continued separation of API

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,5 @@
 const API = require('../helpers/api.js');
 
-const Birds = require('../helpers/birds.js');
-const BirdyPets = require('../helpers/birdypets.js');
-const Cache = require('../helpers/cache.js');
-const Members = require('../helpers/members.js');
-const MemberPets = require('../helpers/memberpets.js');
 const Middleware = require('../helpers/middleware.js');
 const Redis = require('../helpers/redis.js');
 
@@ -17,59 +12,6 @@ const {
 } = require('google-auth-library');
 
 const auth = new GoogleAuth();
-
-const birdsPerPage = 24;
-
-router.get('/birdypedia', async (req, res) => {
-  var page = --req.query.page * birdsPerPage;
-  var wishlist = req.session.user ? await Cache.get('wishlist', req.session.user) : [];
-  var output = [];
-
-  if (req.query.family) {
-    var birds = Birds.fetch("family", req.query.family);
-  } else {
-    var birds = Birds.all().filter((bird) => req.query.adjectives ? bird.adjectives.includes(req.query.adjectives) : true);
-  }
-
-  if (req.query.search) {
-    birds = birds.filter((bird) => bird.commonName.toLowerCase().includes(req.query.search.toLowerCase()));
-  }
-
-  var totalPages = birds.length;
-
-  birds.sort((a, b) => a.commonName.localeCompare(b.commonName));
-
-  for (var i = page, len = Math.min(page + birdsPerPage, birds.length); i < len; i++) {
-    birds[i].wishlisted = wishlist[birds[i].family] ? wishlist[birds[i].family].includes(birds[i].speciesCode) : false;
-    birds[i].variants = BirdyPets.findBy('speciesCode', birds[i].speciesCode).filter((birdypet) => !birdypet.special);
-
-    if (req.session.user) {
-      for (var variant of birds[i].variants) {
-        await Redis.fetch('memberpet', {
-          'FILTER': `@member:{${req.session.user}} @birdypetId:{${variant.id}}`,
-          'COUNT': true
-        }).then((response) => {
-          variant.hatched = response.count;
-        });
-      }
-
-      if (birds[i].variants.length > 0) {
-        birds[i].variants.sort((a, b) => Number(b.hatched) - Number(a.hatched));
-      } else {
-        birds[i].SKIP = true;
-      }
-    }
-
-    if (birds[i].variants.length > 0) {
-      output.push(birds[i]);
-    }
-  }
-
-  res.json({
-    totalPages: Math.ceil(totalPages / birdsPerPage),
-    results: output
-  });
-});
 
 router.post('/flocks/:flock/:memberpet', Middleware.isLoggedIn, Middleware.entityExists, Middleware.userOwnsEntity, (req, res) => {
   var index = req.entities['memberpet'].flocks ? req.entities['memberpet'].flocks.indexOf(req.entities['flock']._id) : -1;
