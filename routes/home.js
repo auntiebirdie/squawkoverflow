0aac768678e493aa7824d6a30b7a228a8cf429c3
seppukawaii <seppukawaii@gmail.com>
2021-11-30 01:15:45 +0000

    Fix login route

--- routes/home.js
+++ routes/home.js
@@ -10,9 +10,13 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/login', (req, res) => {
-	API.call('login', 'POST', req.query).then( (id) => {
-		console.log(id);
-	});
+  if (req.query.code || req.query.konami) {
+    API.call('login', 'POST', req.query).then((id) => {
+      console.log(id);
+    });
+  } else {} else {
+    res.redirect("https://discord.com/api/oauth2/authorize?client_id=885956624777351199&redirect_uri=https%3A%2F%2Fsquawkoverflow.com%2Flogin&response_type=code&scope=identify");
+  }
 });
 
 router.get('/logout', (req, res) => {
