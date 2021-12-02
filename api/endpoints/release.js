e093e811a28d86402ef36ed1b30bc06d2892a414
seppukawaii <seppukawaii@gmail.com>
2021-12-02 20:03:50 +0000

    Fix release for real

--- api/endpoints/release.js
+++ api/endpoints/release.js
@@ -33,8 +33,7 @@ module.exports = async (req, res) => {
     };
 
     await memberpet.delete();
-
-    await Counters.increment(-1, 'species', member.id, memberpet.species.speciesCode);
+    await Counters.increment(-1, 'species', member.member, memberpet.species.speciesCode);
   }
 
   if (birdypet) {
