df3427eeb487733067680e35062d576c65311f67
seppukawaii <seppukawaii@gmail.com>
2021-12-09 01:34:17 +0000

    fix wishlist

--- api/endpoints/birdypedia.js
+++ api/endpoints/birdypedia.js
@@ -2,46 +2,46 @@ const Birds = require('../collections/birds.js');
 const Bird = require('../models/bird.js');
 
 module.exports = async (req, res) => {
-	var birdsPerPage = 24;
-    var page = (--req.query.page || 0) * birdsPerPage;
-    var output = [];
+  var birdsPerPage = 24;
+  var page = (--req.query.page || 0) * birdsPerPage;
+  var output = [];
 
-    if (req.query.family) {
-      var birds = Birds.fetch("family", req.query.family);
-    } else {
-      var birds = Birds.all().filter((bird) => req.query.adjectives ? bird.adjectives.includes(req.query.adjectives) : true);
-    }
+  if (req.query.family) {
+    var birds = Birds.fetch("family", req.query.family);
+  } else {
+    var birds = Birds.all().filter((bird) => req.query.adjectives ? bird.adjectives.includes(req.query.adjectives) : true);
+  }
 
-    if (req.query.search) {
-      birds = birds.filter((bird) => bird.commonName.toLowerCase().includes(req.query.search.toLowerCase()));
-    }
+  if (req.query.search) {
+    birds = birds.filter((bird) => bird.commonName.toLowerCase().includes(req.query.search.toLowerCase()));
+  }
 
-    var totalPages = birds.length;
-    var promises = [];
+  var totalPages = birds.length;
+  var promises = [];
 
-    birds.sort((a, b) => a.commonName.localeCompare(b.commonName));
+  birds.sort((a, b) => a.commonName.localeCompare(b.commonName));
 
-    for (let i = page, len = Math.min(page + birdsPerPage, birds.length); i < len; i++) {
-	    let bird = new Bird(birds[i].speciesCode);
+  for (let i = page, len = Math.min(page + birdsPerPage, birds.length); i < len; i++) {
+    let bird = new Bird(birds[i].speciesCode);
 
-	    promises.push(bird.fetch({
-		    include: ['illustrations', 'memberData'],
-		    member: req.query.loggedInUser
-	    }));
+    promises.push(bird.fetch({
+      include: ['illustrations', 'memberData'],
+      member: req.query.loggedInUser
+    }));
 
-        output.push(bird);
-    }
+    output.push(bird);
+  }
 
-    await Promise.all(promises).then(() => {
-	    if (req.query.loggedInUser) {
+  await Promise.all(promises).then(() => {
+    if (req.query.loggedInUser) {
       for (let bird of output) {
         bird.illustrations.sort((a, b) => (a.hatched === b.hatched) ? 0 : a.hatched ? -1 : 1);
       }
-	    }
+    }
 
-      res.json({
-        totalPages: Math.ceil(totalPages / birdsPerPage),
-        results: output
-      });
+    res.json({
+      totalPages: Math.ceil(totalPages / birdsPerPage),
+      results: output
     });
+  });
 };
