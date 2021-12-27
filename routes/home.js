919d32b174d397565a388bb7fcdd05d59953d59a
seppukawaii <seppukawaii@gmail.com>
2021-12-27 23:27:43 +0000

    force session save

--- routes/home.js
+++ routes/home.js
@@ -12,12 +12,15 @@ router.get('/', async (req, res) => {
 router.get('/login', (req, res) => {
   if (req.query.code || req.query.konami) {
     API.call('login', 'POST', req.query).then((id) => {
-	    req.session.user = id;
+      req.session.user = id;
 
-	    res.redirect('/');
+      req.session.save((err) => {
+
+        res.redirect('/');
+      });
     });
   } else {
-    var redirectUri = process.env.DEV ? 'http%3A%2F%2F35.208.110.100' :  'https%3A%2F%2Fsquawkoverflow.com';
+    var redirectUri = process.env.DEV ? 'http%3A%2F%2F35.208.110.100' : 'https%3A%2F%2Fsquawkoverflow.com';
     res.redirect(`https://discord.com/api/oauth2/authorize?client_id=885956624777351199&redirect_uri=${redirectUri}%2Flogin&response_type=code&scope=identify`);
   }
 });
