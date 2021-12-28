30ffc6be813848508cfc52baeaa9623f22aebc85
seppukawaii <seppukawaii@gmail.com>
2021-12-28 14:59:20 +0000

    add counter refresh to background

--- api/endpoints/_refresh.js
+++ api/endpoints/_refresh.js
@@ -1,3 +1,5 @@
+const BirdyPet = require('../models/birdypet.js');
+const Cache = require('../helpers/cache.js');
 const Counters = require('../helpers/counters.js');
 const Members = require('../collections/members.js');
 const Search = require('../helpers/search.js');
@@ -71,13 +73,45 @@ module.exports = async (req, res) => {
       search: ''
     }));
 
-    promises.push(Search.get('BirdyPet', {
-      member: member.id,
-      page: 1,
-      sort: 'hatchedAt',
-      family: '',
-      flock: member.flock
-    }));
+    if (member.flock) {
+      promises.push(Search.get('BirdyPet', {
+        member: member.id,
+        page: 1,
+        sort: 'hatchedAt',
+        family: '',
+        flock: member.flock
+      }));
+    }
+
+    await Cache.get('aviary', member.id).then(async (results) => {
+      var start = 0;
+      var end = results.length;
+
+      do {
+        let promises = [];
+
+        for (let i = start, len = Math.min(start + 250, end); i < len; i++, start++) {
+          let birdypet = new BirdyPet(results[i]);
+
+          promises.push(birdypet.fetch());
+        }
+
+        await Promise.all(promises).then((birdypets) => {
+          let promises = [];
+
+          for (let birdypet of birdypets) {
+            promises.push(Counters.refresh('birdypets', member.id, birdypet.illustration.id));
+          }
+
+          return Promise.all(promises);
+        });
+
+        promises = [];
+      }
+      while (start < end);
+
+      return true;
+    });
 
     promises.push(member.set({
       lastRefresh: Date.now()
---
+++ test.js
@@ -0,0 +1,8 @@
+var func = require('./api/endpoints/_refresh');
+
+func({
+	query: {
+		member: "121294882861088771"
+	}
+}, {
+});
