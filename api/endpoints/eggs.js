d0e62045a8a2bc1f6000922944344d1eab179fde
seppukawaii <seppukawaii@gmail.com>
2022-01-15 16:56:02 -0500

    hopefully fix egg guide counters

--- api/endpoints/eggs.js
+++ api/endpoints/eggs.js
@@ -1,26 +1,11 @@
 const Counters = require('../helpers/counters.js');
+const Database = require('../helpers/database.js');
 
 module.exports = (req, res) => {
-  let promises = [];
-
-  let eggs = Object.entries(require('../data/eggs.json')).filter((egg) => egg[0].startsWith(req.query.firstLetter)).map(([egg, data]) => {
-    let promise = req.query.loggedInUser ? Counters.get('eggs', req.query.loggedInUser, egg) : 0;
-
-    promises.push(promise);
-
-    return {
-      name: egg,
-      speciesTotal: data.species.length,
-      memberTotal: promise
-    }
-  });
-
-  Promise.allSettled(promises).then((results) => {
-    for (let egg of eggs) {
-      if (egg.memberTotal) {
-        egg.memberTotal.then((value) => {
-          egg.memberTotal = value;
-        });
+  Database.query('SELECT * FROM adjectives WHERE adjective LIKE ?', [`${req.query.firstLetter}%`]).then(async (eggs) => {
+    if (req.query.loggedInUser) {
+      for (let egg of eggs) {
+        egg.memberTotal = await Counters.get('eggs', req.query.loggedInUser, egg.adjective);
       }
     }
 
