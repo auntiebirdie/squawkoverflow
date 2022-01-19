ae054c6ab4cfa17a818870336f65a456f69e85d2
seppukawaii <seppukawaii@gmail.com>
2022-01-19 15:53:33 -0500

    add birdy buddy friendship endpoint

---
+++ api/endpoints/_birdybuddy.js
@@ -0,0 +1,20 @@
+const Member = require('../models/member.js');
+
+module.exports = async (req, res) => {
+  return new Promise(async (resolve, reject) => {
+      let member = new Member(req.query.loggedInUser);
+
+      member.fetch({
+        include: 'birdyBuddy'
+      });
+
+      if (member.birdyBuddy?.variant) {
+        await member.birdyBuddy.set({
+          friendship: member.birdyBuddy.friendship + (req.query.friendship * 1)
+        });
+      }
+    }
+
+    return res.send(member.birdyBuddy);
+  });
+};
