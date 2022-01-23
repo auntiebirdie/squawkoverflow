a16af4bda9e710f9d94b21a650af48f8663b852c
seppukawaii <seppukawaii@gmail.com>
2022-01-23 01:00:48 -0500

    fix typo

--- api/endpoints/_cockadoodledoo.js
+++ api/endpoints/_cockadoodledoo.js
@@ -3,7 +3,7 @@ const Database = require('../helpers/database.js');
 module.exports = async (req, res) => {
   return new Promise(async (resolve, reject) => {
     /* New Day New Bait! */
-    await Database.query('UPDATE counters SET 0 WHERE type = "bait"');
+    await Database.query('UPDATE counters SET `count` = 0 WHERE type = "bait"');
   }).then(() => {
     return res.sendStatus(200);
   });
