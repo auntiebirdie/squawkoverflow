698f27b8eb5266163d699fba72adcdf0aede07a5
seppukawaii <seppukawaii@gmail.com>
2022-03-02 19:13:44 +0000

    fix event endpoint

--- api/endpoints/_events.js
+++ api/endpoints/_events.js
@@ -3,13 +3,12 @@ const secrets = require('../secrets.json');
 const Database = require('../helpers/database.js');
 const https = require('https');
 
-(() => {
-  //module.exports = async (req, res) => {
+module.exports = async (req, res) => {
   return new Promise((resolve, reject) => {
-	  var timeMin = new Date();
-	  var timeMax = new Date();
+    var timeMin = new Date();
+    var timeMax = new Date();
 
-	  timeMax.setMonth(timeMin.getMonth() + 1);
+    timeMax.setMonth(timeMin.getMonth() + 1);
 
     https.get(`https://www.googleapis.com/calendar/v3/calendars/rlcqglcgbhotqam5inja7jttjc@group.calendar.google.com/events?key=${secrets.GOOGLE.API_KEY}&timeMin=${timeMin.toISOString()}&timeMax=${timeMax.toISOString()}`, (response) => {
       var data = "";
@@ -19,26 +18,24 @@ const https = require('https');
           return data += chunk;
         })
         .on('end', async () => {
-		let promises = [];
+          let promises = [];
 
-		data = JSON.parse(data);
+          data = JSON.parse(data);
 
-		for (let item of data.items) {
-			promises.push(Database.replace('events', {
-				id: item.iCalUID,
-				name: item.summary,
-				description : item.description.replace(/\<em\>\<i\>/g, '*').replace(/\<\/i\>\<\/em\>/g, '*').replace(/\<br\>/g, "\r\n"),
-				startDate: item.start.date,
-				endDate: item.end.date
-			}));
-		}
+          for (let item of data.items) {
+            promises.push(Database.replace('events', {
+              id: item.iCalUID,
+              name: item.summary,
+              description: item.description.replace(/\<em\>\<i\>/g, '*').replace(/\<\/i\>\<\/em\>/g, '*').replace(/\<br\>/g, "\r\n"),
+              startDate: item.start.date,
+              endDate: item.end.date
+            }));
+          }
 
-		Promise.all(promises).then( () => {
-			console.log('events synchronized!');
-          //res.sendStatus(200);
-		});
+          Promise.all(promises).then(() => {
+            res.sendStatus(200);
+          });
         });
     });
   });
-  //};
-})();
+};
