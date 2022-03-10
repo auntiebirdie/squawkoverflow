6062830d0188a59012f89fc74ae65db2d42c754c
seppukawaii <seppukawaii@gmail.com>
2022-03-10 16:25:55 +0000

    fix sessions to work across subdomains

--- helpers/session.js
+++ helpers/session.js
@@ -1,24 +1,22 @@
-const secrets = require('../secrets.json');
+const {
+  Datastore
+} = require('@google-cloud/datastore');
+const {
+  DatastoreStore
+} = require('@google-cloud/connect-datastore');
 const session = require('express-session');
 
-const DB = secrets.REDIS[process.env.NODE_ENV ? 'PROD' : 'DEV'];
-
-const RedisStore = require('connect-redis')(session);
-const RedisClient = require('redis').createClient({
-  host: DB.HOST,
-  port: DB.PORT,
-  password: DB.PASS
-});
-
 module.exports = session({
-  store: new RedisStore({
-    client: RedisClient
+  store: new DatastoreStore({
+    kind: 'express-sessions',
+    expirationMs: 0,
+    dataset: new Datastore()
   }),
   secret: 'birds are just government drones',
   resave: false,
   saveUninitialized: false,
   cookie: {
-    domain: 'squawkoverflow.com',
+    domain: '.squawkoverflow.com',
     maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
   }
 });
