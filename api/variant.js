fe6de89a3132575a0ffa9e19c980d8a6afe6b791
seppukawaii <seppukawaii@gmail.com>
2022-06-20 13:45:19 +0000

    re-enable tumblr postings

--- api/variant.js
+++ api/variant.js
@@ -13,6 +13,7 @@ const storage = new Storage();
 const bucket = storage.bucket('squawkoverflow');
 const Jimp = require('jimp');
 const uuid = require('short-uuid');
+const Tumblr = require('tumblr.js');
 
 module.exports = async (req, res) => {
   switch (req.method) {
@@ -163,7 +164,35 @@ module.exports = async (req, res) => {
             }]
           });
 
-          res.json(key);
+          if (secrets.ENV == "PROD") {
+            const tumblr = Tumblr.createClient({
+              consumer_key: secrets.TUMBLR.OAUTH_KEY,
+              consumer_secret: secrets.TUMBLR.OAUTH_SECRET,
+              token: secrets.TUMBLR.squawkoverflow.OAUTH_TOKEN,
+              token_secret: secrets.TUMBLR.squawkoverflow.OAUTH_TOKENSECRET
+            });
+
+            let lastEgg = variant.bird.adjectives.pop();
+
+            await new Promise((resolve, reject) => {
+              tumblr.createPhotoPost('squawkoverflow', {
+                type: 'photo',
+                state: 'queue',
+                tags: ['squawkoverflow', variant.bird.commonName, variant.bird.scientificName, 'birds', data.credit].join(',').toLowerCase(),
+                source_url: data.source,
+                source: variant.image,
+                caption: `<h2>A new variant has been added!</h2><p>${variant.bird.commonName} <i>(${variant.bird.scientificName})</i><br><a href="${data.source}">© ${data.credit}</a></p><p>It hatches from ${variant.bird.adjectives.join(', ')}, and ${lastEgg} eggs.</p><p><a href="https://squawkoverflow.com/">squawkoverflow - the ultimate bird collecting game</a><br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;🥚 hatch &nbsp; &nbsp;❤️ collect &nbsp; &nbsp; 🤝 connect</p>`
+              }, function(err, resp) {
+                console.log(err, resp);
+                resolve();
+              });
+
+              res.json(key);
+            });
+          } else {
+
+            res.json(key);
+          }
         } else {
           res.json(data.id);
         }
