f60d59e8459b72d6959a72f831b69043e43a5e5a
seppukawaii <seppukawaii@gmail.com>
2022-06-30 15:19:22 +0000

    distinct id

--- helpers/search.js
+++ helpers/search.js
@@ -43,8 +43,8 @@ class Search {
           filters.push('birdypets.member IS NULL AND birdypets.addedAt <= DATE_SUB(NOW(), INTERVAL 10 MINUTE)');
           break;
         case 'incubator':
-          select.push('variants.id');
-          tables.push('variants', 'LEFT JOIN member_variants ON (variants.id = member_variants.variant)', 'JOIN species ON (variants.species = species.id)');
+          select.push('DISTINCT variants.id');
+          tables.push('variants', 'JOIN member_variants ON (variants.id = member_variants.variant)', 'JOIN species ON (variants.species = species.id)');
           filters.push('member_variants.member = ?');
           params.push(input.loggedInUser);
           break;
