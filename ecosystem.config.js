c80bfa711cf7e5d8f749989e41d73076a5b7042e
seppukawaii <seppukawaii@gmail.com>
2022-07-07 13:40:12 +0000

    add bugs for now

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,14 @@
 module.exports = {
-  apps: [
-    {
+  apps: [{
       name: "squawk",
       script: "app.js",
       instances: 2,
       exec_mode: "cluster"
+    },
+    {
+      name: "bugs",
+      script: "bugs.js",
+      instances: 1
     }
   ]
 }
