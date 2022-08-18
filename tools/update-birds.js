d33322da1e88ce2b5cf1319ad2b73a5a62f4f761
seppukawaii <seppukawaii@gmail.com>
2022-08-18 20:10:54 +0000

    WIP of changelog output

--- tools/update-birds.js
+++ tools/update-birds.js
@@ -6,8 +6,6 @@ const secrets = require('../secrets.json');
 (async () => {
   let orders = [];
   let families = [];
-  let promises = [];
-  let total = 0;
 
   const conn = await mariadb.createConnection({
     host: secrets.DB.HOST,
@@ -20,6 +18,18 @@ const secrets = require('../secrets.json');
   let existingSpecies = await conn.query('SELECT id FROM squawkdata.species').then((results) => results.map((result) => result.id));
   let updatedSpecies = [];
 
+  let changelog = {
+    orders: [],
+    families: {
+      'new': [],
+      'updated': []
+    },
+    species: {
+      'new': [],
+      'updated': []
+    }
+  };
+
   const request = https.get('https://worldbirdnames.org/Multiling%20IOC%2012.2.xlsx', (response) => {
     const chunks = [];
 
@@ -31,6 +41,8 @@ const secrets = require('../secrets.json');
       const id = Object.keys(data[0])[3];
 
       for (let row of data) {
+        console.log(row['Seq.']);
+
         let order = row['Order'].slice(0, 1).toUpperCase() + row['Order'].slice(1).toLowerCase();
         let family = row['Family'];
 
@@ -38,7 +50,7 @@ const secrets = require('../secrets.json');
           let orderCheck = await conn.query('SELECT * from squawkdata.taxonomy WHERE name = ? AND type = "order"', [order]);
 
           if (orderCheck.length == 0) {
-            throw `NEW ORDER: ${order}`;
+            changelog.orders.push(order);
           }
 
           orders.push(order);
@@ -48,37 +60,61 @@ const secrets = require('../secrets.json');
           let familyCheck = await conn.query('SELECT * FROM squawkdata.taxonomy WHERE name = ? AND type = "family"', [family])
 
           if (familyCheck.length == 0) {
-            throw `NEW FAMILY: ${family}`;
+            changelog.families['new'].push(family);
           } else if (familyCheck[0].parent != order) {
-            console.log(`FAMILY CHANGED: ${family}   ${familyCheck[0].parent} -> ${order}`);
+            changelog.families['updated'].push([family, 'order', familyCheck[0].parent, order]);
           }
 
           families.push(family);
         }
 
-        let species = await conn.query('SELECT * FROM squawkdata.species WHERE id = ? OR commonName = ?', [row[id], row['English']]);
+        let species = await conn.query('SELECT * FROM squawkdata.species WHERE id = ? OR commonName = ? OR id IN (SELECT species FROM squawkdata.species_names WHERE name = ?)', [row[id], row['English'], row['English']]);
 
         if (species.length > 0) {
           species = species[0];
-
-          let url = `https://squawkoverflow.com/birdypedia/bird/${species.id.replace(/\s/g, '-')}/edit`;
+          let updated = [];
 
           if (species.id != row[id]) {
-            console.log('CHANGE', `${species.id} -> ${row[id]}`, url);
+            updated.push(['Scientific Name', species.id, row[id]]);
           }
           if (species.commonName != row['English']) {
-
-            console.log('CHANGE', `${species.commonName} -> ${row['English']}`, url);
+            updated.push(['Common Name', species.commonName, row['English']]);
           }
           if (species.family != family) {
+            updated.push(['Family', species.family, family]);
+          }
 
-
-            console.log('CHANGE', `${species.family} -> ${family}`, url);
+          if (updated.length > 0) {
+            changelog.species['updated'].push(species.id, updated);
           }
 
           updatedSpecies.push(species.id);
         } else {
-          console.log('NEW', family, row[id], row['English']);
+          changelog.species['new'].push([row['English'], row[id], family]);
+        }
+      }
+
+      console.log(changelog);
+
+      if (changelog.families.updated.length > 0) {
+        console.log('*** UPDATED FAMILIES ***');
+
+        for (let family of changelog.families.updated) {
+          if (family[1] == 'order') {
+            console.log(`${family}\r\n - moved from ${family[2]} to ${family[3]}`);
+          }
+        }
+      }
+
+      if (changelog.species.updated.length > 0) {
+        console.log('*** UPDATED SPECIES ***');
+
+        for (let species of changelog.species.updated) {
+          console.log(`${species[0]}`);
+
+          for (let updated of species[1]) {
+            console.log(` - ${updated[0]} changed from ${updated[1]} to ${updated[2]}`);
+          }
         }
       }
 
