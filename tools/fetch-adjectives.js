dc39e9c25e0bc5ec34db5646547fb7f6c0881bc5
Auntie Birdie <birdieauntie@gmail.com>
2024-09-27 22:17:17 +0000

    add tool to more easily update with IOC list

--- tools/fetch-adjectives.js
+++ tools/fetch-adjectives.js
@@ -1,17 +1,75 @@
+const Compromise = require('compromise');
 const Database = require('../helpers/database.js');
-const ogs = require('open-graph-scraper');
+const Redis = require('../helpers/redis.js');
 
-(async () => {
-  let bird = await Database.query('SELECT code FROM species WHERE id = ? LIMIT 1', [process.argv[2] + " " + process.argv[3]]);
+// https://nodejs.org/en/learn/command-line/accept-input-from-the-command-line-in-nodejs
+const readline = require('node:readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-  if (bird) {
-	  ogs({ url : 'https://ebird.org/species/rehwea3' }).then((data) => {
-		  console.log(data);
-	  }).catch((err) => {
-	    console.log(err);
-    });
-  } else {
-    console.log(`https://squawkoverflow.com/birdypedia/bird/${process.argv[2]}s-${process.argv[3]}/edit`);
-    process.exit(0);
+rl.question(`Scientific name: `, async (scientificName) => {
+  // Make sure this bird exists.
+  let bird = await Database.query('SELECT id, code FROM species WHERE id = ? LIMIT 1', [scientificName]);
+
+  // If it doesn't...
+  if (!bird) {
+    // Print an error and exit.
+    return console.error(`Couldn't find a species with the scientific name ${scientificName}.`);
   }
-})();
+
+  if (bird.code) {
+    console.log(`https://ebird.org/species/${bird.code}`);
+  }
+
+  // Prompt user for eBird description.
+  rl.question(`Paste description for parsing: `, async (description) => {
+    rl.close();
+
+    // Fetch adjectives from description.
+    let adjectives = Compromise(description).adjectives().out('array').map((adjective) => {
+      // Sanitize the adjectives (lowercase, remove non-alphabetical characters)
+      return adjective.toLowerCase().replace(/[^a-z]/, "")
+    }).filter((value, index, array) => {
+      // Remove duplicates.
+      // For example, if an array has ["yellow", "red", "yellow"], the first instance of yellow will match the indexOf, which
+      // returns the index of the first instance of a value. The second yellow won't match, and will be removed.
+      return array.indexOf(value) === index
+    })
+
+    // For each adjective found...
+    for (let adjective of adjectives) {
+      console.log(adjective)
+
+      // Check if this species already has this adjective...
+      let species_adjective = await Database.query('SELECT COUNT(*) AS count FROM species_adjectives WHERE species = ? AND adjective = ?', [bird.id, adjective]);
+
+	    console.log(species_adjective)
+
+      // If it does not...
+      if (species_adjective[0].count == 0) {
+        // If this adjective doesn't exist, add it!  Otherwise, do nothing.
+        await Database.query('INSERT IGNORE INTO adjectives VALUES (?, 0, NULL, NULL)', [adjective]);
+
+        // Add the adjective to the bird.
+        await Database.query('INSERT INTO species_adjectives VALUES (?, ?)', [bird.id, adjective]);
+
+        // Update the adjective "number of species" count.
+        await Database.query('UPDATE adjectives SET numSpecies = (SELECT COUNT(*) FROM species_adjectives WHERE adjective = ?) WHERE adjective = ?', [adjective, adjective]);
+
+        // Update member counts.
+        await Database.query('REPLACE INTO counters (SELECT member, "eggs", adjective, COUNT(DISTINCT species.id) FROM species JOIN species_adjectives ON species.id = species_adjectives.species JOIN counters ON species.id = counters.id WHERE counters.type = "birdypedia" AND adjective = ? GROUP BY member, adjective)', [adjective]);
+      }
+    }
+
+    // Flush the Redis cache.
+    await Redis.sendCommand(['KEYS', 'eggs:*']).then((results) => {
+      for (let result of results) {
+        Redis.sendCommand(['DEL', result]);
+      }
+    });
+
+    process.exit(0);
+  });
+});
--- tools/update-birds.js
+++ tools/update-birds.js
@@ -1,71 +1,200 @@
+const Axios = require('axios');
+const Cheerio = require('cheerio');
 const Database = require('../helpers/database.js');
 const Redis = require('../helpers/redis.js');
-const xlsx = require('xlsx');
 
-const workbook = xlsx.readFile('Multiling IOC 13.1_c.xlsx');
-const IOC = xlsx.utils.sheet_to_json(workbook.Sheets['List']);
+// https://nodejs.org/en/learn/command-line/accept-input-from-the-command-line-in-nodejs
+const readline = require('node:readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-(async () => {
-  let iocBirds = [];
+// A version is required to know what version we're upgrading to.
+const version = process.argv[2];
 
-  for (let row of IOC) {
-    console.log(row['Seq.']);
+// If a version isn't specified...
+if (!version) {
+  // Print the error message and quit.
+  return console.error("Please specify the version to update to as a command-line argument, e.g. node update-birds 14.2");
+}
 
-    let _ID = row['IOC_13.1'];
-    let _NAME = row['English name'];
+// If the version specified is less than 13.1...
+if (version * 1 < 13.1) {
+  // Print the error message and quit.
+  return console.error("This version has already been processed. There is no need to do it again!");
+}
 
+// Fetch the Species Update page from WorldBirdNames.org
+const url = "https://www.worldbirdnames.org/new/updates/species-updates/";
+
+console.log(`Fetching content from ${url} (this will take a moment to load)...`);
+
+Axios({
+  url
+}).then(async (response) => {
+  const $ = Cheerio.load(response.data);
+
+  // Collect all the tables on the page.
+  const containers = $('div.fl-html');
+  var table;
+
+  // Loop through each container.
+  for (let container of containers) {
+    // Fetch the header in the container.
+    let header = $('h3', container);
+
+    // If this is the one we're looking for...
+    if (header.text() == `Species Updates IOC Version ${version}`) {
+      // Save it and stop looping.
+      table = $('table', container);
+      break;
+    }
+  }
+
+  // If no matching table was found for the specified version...
+  if (!table) {
+    return console.error(`No table was found for version ${version}. Are you sure it exists?`);
+  }
+
+  // Free up memory before we continue.
+  delete response;
+  delete containers;
+
+  // Collect all the rows in the table's tbody.
+  const rows = $('tbody tr', table);
+
+  // Loop through each row.
+  for (let row of rows) {
+    // Some rows have multiple species in the same row :(
+    // Let's operate under the assumption that every row could be comma-delimited.
+    let species_common_names = $('td:first', row).text().split(',')
+    let species_scientific_names = $('td:nth-child(2)', row).text().split(',')
+
+    // For each species in this row...
+    for (let i = 0, len = species_common_names.length; i < len; i++) {
+      let data = {
+        common_name: species_common_names[i].trim(),
+        scientific_name: species_scientific_names[i].trim(),
+        notes: $('td:nth-child(6)', row).text().trim()
+      };
+
+      let action = $('td:nth-child(3)', row).text().trim();
+
+      switch (action) {
+        case "ADD":
+          await addSpecies(data);
+          break;
+        case "DEL":
+          await deleteSpecies(data);
+          break;
+        default:
+          return console.error(`Unknown action ${action} for ${data.common_name}`);
+      }
+    }
+  }
+
+  return process.exit(0);
+})
+
+// Adds a new species, if it doesn't already exist.
+function addSpecies(data) {
+  return new Promise(async (resolve, reject) => {
+    // Check if this species is already in the Birdypedia.
     let bird = await Database.query('SELECT DISTINCT id, commonName FROM species JOIN species_names ON (species.id = species_names.species) WHERE species.id = ? OR species.commonName = ? OR (species_names.name = ? AND species_names.lang = "zz") LIMIT 1',
-      [_ID, _NAME, _ID]
+      [data.scientific_name, data.common_name, data.scientific_name]
     );
 
-    iocBirds.push(_ID);
-
+    // If this bird does not exist...
     if (!bird) {
-      console.log("Adding new bird...");
-      await Database.query('INSERT INTO species VALUES (?, NULL, ?, ?, ?)', [_ID, row['Family'], row['English name'], _ID]);
-      await Database.query('INSERT INTO variants VALUES (?, ?, "", "", "n/a", "n/a", 0, NULL, NULL, -1, NULL, NULL, NOW(), NOW())', [Database.key(), _ID]);
-      await Database.query('INSERT INTO species_adjectives VALUES (?, "new")', [_ID]);
-      await Database.query('INSERT INTO species_names VALUES (?, ?, "en")', [_ID, row['English name']]);
-      await Database.query('INSERT INTO species_names VALUES (?, ?, "zz")', [_ID, _ID]);
-    } else {
-      if (bird.id != _ID) {
-        console.log("Updating existing bird (scientific name)...");
-        await Database.query('UPDATE counters SET id = ? WHERE id = ?', [_ID, bird.id]);
-        await Database.query('UPDATE member_unlocks SET species = ? WHERE species = ?', [_ID, bird.id]);
-        await Database.query('UPDATE variants SET species = ? WHERE species = ?', [_ID, bird.id]);
-        await Database.query('UPDATE species_adjectives SET species = ? WHERE species = ?', [_ID, bird.id]);
-        await Database.query('UPDATE wishlist SET species = ? WHERE species = ?', [_ID, bird.id]);
-        await Database.query('INSERT IGNORE INTO species_names VALUES (?, ?, "zz")', [_ID, bird.id]);
-        await Database.query('UPDATE species SET id = ?, scientificName = ? WHERE id = ?', [_ID, _ID, bird.id]);
-      }
+      console.log(`\nAdding ${data.common_name} (${data.scientific_name})...`);
 
-      if (bird.commonName != _NAME) {
-        let isLocalized = await Database.query('SELECT COUNT(*) total FROM species_names WHERE species = ? AND name = ? LIMIT 1', [_ID, row['English name']]);
+      // Prompt for the Family.
+      let family = await promptUser("What is the family for this bird?")
 
+      // Add this species to the Birdypedia.
+      await Database.query('INSERT INTO species VALUES (?, NULL, ?, ?, ?)', [data.scientific_name, family, data.common_name, data.scientific_name]);
 
-        if (isLocalized.total == 0) {
-          console.log("Updating existing bird (common name)...");
+      // Add a placeholder image.
+      await Database.query('INSERT INTO variants VALUES (?, ?, "", "", "n/a", "n/a", 0, NULL, NULL, -1, NULL, NULL, NOW(), NOW())', [Database.key(), data.scientific_name]);
 
-          await Database.query('INSERT IGNORE INTO species_names VALUES (?, ?, "en")', [_ID, bird.commonName]);
-          await Database.query('INSERT IGNORE INTO species_names VALUES (?, ?, "en")', [_ID, row['English name']]);
-          await Database.query('UPDATE species SET commonName = ? WHERE id = ?', [row['English name'], _ID]);
-        }
-      }
+      // Add the "new" adjective.
+      await Database.query('INSERT INTO species_adjectives VALUES (?, "new")', [data.scientific_name]);
+
+      // Add the common and scientific names to the species_names table.
+      await Database.query('INSERT INTO species_names VALUES (?, ?, "en")', [data.scientific_name, data.common_name]);
+      await Database.query('INSERT INTO species_names VALUES (?, ?, "zz")', [data.scientific_name, data.scientific_name]);
     }
-  }
 
-  let squawkBirds = await Database.query('SELECT id, commonName FROM species');
-  let extinctBirds = ["Alectroenas payandeei", "Amazona martinicana", "Archaeopteryx lithographica", "Psittacosaurus mongoliensis", "Amazona violacea", "Aplonis ulietensis", "Foudia delloni", "Nesoenas cicur", "Nesoenas duboisi", "Tribonyx hodgenorum", "Porphyrio caerulescens", "Porphyrio kukwiedei", "Porphyrio paepae", "Psittacara labati", "Tribonyx hodgenorum", "Chenonetta finschi", "Columba thiriouxi"];
-  let domesticBirds = ["Anas platyrhynchos domesticus", "Columba livia domestica", "Gallus gallus domesticus", "Serinus canaria domesticus"];
-  let hybridBirds = ["Craspedophora duivenbodei", "Parotia duivenbodei"];
+    return resolve();
 
-  for (let bird of squawkBirds) {
-    if (!iocBirds.includes(bird.id) && !extinctBirds.includes(bird.id) && !domesticBirds.includes(bird.id) && !hybridBirds.includes(bird.id)) {
-      console.log(bird);
-    }
-  }
-
-  Redis.sendCommand(['FLUSHDB']).then((results) => {
-    process.exit(0);
   });
-})();
+}
+
+// Removes a species by merging it into another.
+function deleteSpecies(data) {
+  return new Promise(async (resolve, reject) => {
+    // Check if this species is already in the Birdypedia.
+    let bird = await Database.query('SELECT DISTINCT id, commonName FROM species JOIN species_names ON (species.id = species_names.species) WHERE species.id = ? OR species.commonName = ? OR (species_names.name = ? AND species_names.lang = "zz") LIMIT 1',
+      [data.scientific_name, data.common_name, data.scientific_name]
+    );
+
+    // If this bird does not exist...
+    if (bird) {
+      console.log(`\nRemoving ${data.common_name} (${data.scientific_name})...`);
+      console.log(data.notes);
+
+      // Prompt for species to merge with.
+      let mergeName = await promptUser("What is the scientific name of the species to merge this one into?");
+
+      // Make sure the specified bird exists.
+      let mergeBird = await Database.query('SELECT DISTINCT id, commonName FROM species JOIN species_names ON (species.id = species_names.species) WHERE species.id = ? OR (species_names.name = ? AND species_names.lang = "zz") LIMIT 1',
+        [mergeName, mergeName]
+      );
+
+      // If the specified bird doesn't exist...
+      if (!mergeBird) {
+        console.error(`${mergeName} doesn't exist. Please try again.`)
+        // Call this function again to start it over.
+        deleteSpecies.then(resolve);
+      }
+
+      // Update any variants, moving them to the new species and setting the subspecies.
+      await Database.query('UPDATE variants SET species = ?, subspecies = ? WHERE species = ?', [mergeBird.id, data.scientific_name.split(' ').pop(), data.scientific_name]);
+
+      // Remove the old species from wishlists.
+      await Database.query('DELETE FROM wishlist WHERE species = ?', [data.scientific_name]);
+
+      // Update member counters for the species being merged into.
+      await Database.query('REPLACE INTO counters (SELECT `member`, `type`, ?, MAX(`count`) FROM counters WHERE type = "birdypedia" AND id IN (?, ?) GROUP BY `member`)', [mergeBird.id, data.scientific_name, mergeBird.id]);
+
+      // Remove old species counters.
+      await Database.query('DELETE FROM counters WHERE id = ?', [data.scientific_name]);
+
+      // Clean up old species adjectives.
+      await Database.query('DELETE FROM species_adjectives WHERE species = ?', [data.scientific_name]);
+
+      // Add the old species names to the species being merge dinto.
+      await Database.query('UPDATE species_names SET species = ? WHERE species = ?', [mergeBird.id, data.scientific_name]);
+
+      // Remove the species. :(
+      await Database.query('DELETE FROM species WHERE id = ?', [data.scientific_name]);
+
+      // If someone unlocked the old species but not the species being merged into, unlock it for them.
+      await Database.query('UPDATE IGNORE member_unlocks SET species = ? WHERE species = ?', [mergeBird.id, data.scientific_name]);
+
+      // Clean up old species unlocks.
+      await Database.query('DELETE FROM member_unlocks WHERE species = ?', [data.scientific_name]);
+    }
+
+    return resolve();
+  });
+}
+
+function promptUser(question) {
+  return new Promise((resolve, reject) => {
+    rl.question(`${question} > `, (answer) => {
+      resolve(answer);
+    });
+  });
+}
